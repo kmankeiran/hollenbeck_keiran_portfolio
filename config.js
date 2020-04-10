0c81522f2143ed962ac1c6c04ebcce9e3a44f644
tvanrys <tvanrys@fanshaweonline.ca>
2020-04-10 15:25:53 -0400

    updating config etc

--- config.js
+++ config.js
@@ -1,25 +1,25 @@
-const config = {
-    host: "localhost",
-    port: 3306, // 8889 for mac
-    uname: "root",
-    pword: "",
-    database: "db_portfolio"
-}
-
-module.exports = config;
-
-
-
-
-
-
-
 // const config = {
-//     host: "us-cdbr-iron-east-05.cleardb.net",
-//     // port: 3306, // 8889 for mac
-//     uname: "b69b5877f17994",
-//     pword: "d711695a",
-//     database: "heroku_29b054157d1aacc"
+//     host: "localhost",
+//     port: 3306, // 8889 for mac
+//     uname: "root",
+//     pword: "",
+//     database: "db_portfolio"
 // }
 
 // module.exports = config;
+
+
+
+
+
+
+
+const config = {
+    host: "us-cdbr-iron-east-01.cleardb.net",
+    // port: 3306, // 8889 for mac
+    uname: "b69b5877f17994",
+    pword: "d711695a",
+    database: "heroku_29b054157d1aacc"
+}
+
+module.exports = config;
